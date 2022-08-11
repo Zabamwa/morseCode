b86a342684ee89ff5d482a9f43704be44dcd9b1a
Konrad <zabinski.konrad@gmail.com>
2022-08-11 16:21:31 +0200

    morseCode app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import './App.css';
+import { MorseLetter } from "./utils/const";
+
 
 function App() {
-  return (
+    const [pressed, setPressed] = useState<boolean>(false);
+    const [decode, setDecode] = useState<boolean>(false);
+    const [morse, setMorse] = useState<string>('');
+    const [errors, setErrors] = useState<string[]>([]);
+    const [text, setText] = useState<string>('');
+    const [key, setKey] = useState<string>('');
+    const longPress = useRef(false);
+
+    const decodeMorse = useCallback(() => {
+        const morseCode = morse.split(' ');
+        setText('');
+        setErrors([]);
+        morseCode.forEach((code:string) => {
+            if(code) {
+                if (code && MorseLetter[code]) {
+                    setText(prevState => prevState + MorseLetter[code]);
+                } else {
+                    setErrors(prevState => ([...prevState, code]))
+                }
+            }
+        });
+    }, [morse]);
+
+    const codingMorse = (code:string) => {
+        setMorse(prevState => prevState + code);
+        longPress.current = true;
+    };
+
+    const handlePress = (key: string) => {
+        setKey(key);
+        setPressed(true);
+    };
+    useEffect(() => {
+        if(key === 'Space') {
+            let time: ReturnType<typeof setTimeout>;
+            let letterSpace: ReturnType<typeof setTimeout>;
+            if (pressed) {
+                time = setTimeout(() => codingMorse('-'), 1000);
+            } else {
+                letterSpace = setTimeout(() => codingMorse(' '), 1000);
+            }
+            return () => {
+                if (pressed && !longPress.current) {
+                    setMorse(prevState => prevState + '.');
+                }
+                setDecode(true);
+                clearTimeout(time);
+                clearTimeout(letterSpace);
+                longPress.current = false;
+            }
+        }
+    }, [pressed, key]);
+
+    useEffect(() => {
+        if(decode){
+            decodeMorse();
+            setDecode(false);
+        }
+    }, [decode, decodeMorse]);
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button autoFocus={true} onKeyDown={(e:React.KeyboardEvent) => handlePress(e.code)} onKeyUp={()=>setPressed(false)}>Press Spacebar</button>
+        <p>Morse Code: {morse}</p>
+        <p>Decode Text: {text}</p>
+        <p>Wrong Codes: {errors.map(error => (
+            `${error} , `
+        ))}</p>
     </div>
   );
 }
